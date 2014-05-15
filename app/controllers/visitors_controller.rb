dbfc2d26c5d93c5e11d23941052f6f56c91df3e8
Eric Schulz <eschulz2@gmail.com>
2014-05-15 18:03:52 -0500

    visitor improved

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -8,7 +8,7 @@ class VisitorsController < ApplicationController
     @visitor = Visitor.new(secure_params)
     if @visitor.valid?
       @visitor.subscribe
-      flash[:notice] = "Signed up #{@visitor.email}."
+      flash[:notice] = "Got it, #{@visitor.email}."
       redirect_to root_path
     else
       render :new
--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,7 +1,7 @@
 class Visitor < ActiveRecord::Base
   has_no_table
   column :email, :string
-  # validates_presence_of :email
+  validates_presence_of :email
   # validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 
   def subscribe
