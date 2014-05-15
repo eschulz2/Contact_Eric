5ec4dbd0365f9260b6a4b7f49df9289524021ba6
Eric Schulz <eschulz2@gmail.com>
2014-05-15 17:52:25 -0500

    edited domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: ericschulz.info,
+    domain: "ericschulz.info",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: Rails.application.secrets.email_provider_username,
